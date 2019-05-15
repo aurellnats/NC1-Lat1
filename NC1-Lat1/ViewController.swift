88615b4cd39ab27be0c9717191041d5ffbea768b
Aurelia <aurelianatashaa@yahoo.com>
2019-05-15 08:14:54 +0700

    Initial Commit

---
+++ NC1-Lat1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC1-Lat1
+//
+//  Created by aurelia  natasha on 15/05/19.
+//  Copyright © 2019 aurelia  natasha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
